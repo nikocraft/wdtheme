f8996f2d4c58431b675ac9a38121085582fe1bf9
Niko Craft <nermion@gmail.com>
2019-12-15 21:37:41 +0100

    fix: correctly call the function this time

--- src/views/includes/seo.blade.php
+++ src/views/includes/seo.blade.php
@@ -3,14 +3,14 @@
         @push('seo-meta-tags')
             <title>{{ $content->title }} - {{ get_website_setting('website.title') }}</title>
             <meta name="title" content="{{ $content->title }} | {{ get_website_setting('website.title') }}">
-            <meta name="description" content="{{ get_seo_description($content, get_theme_settings('seo.meta.description')) }}">
+            <meta name="description" content="{{ get_seo_description($content, get_theme_setting('seo.meta.description')) }}">
         @endpush
 
         @push('open-graph-meta-tags')
             <meta property="og:type" content="website">
             <meta property="og:url" content="https://">
             <meta property="og:title" content="{{ $content->title }} | {{ get_website_setting('website.title') }}">
-            <meta property="og:description" content="{{ get_seo_description($content, get_theme_settings('seo.meta.description')) }}">
+            <meta property="og:description" content="{{ get_seo_description($content, get_theme_setting('seo.meta.description')) }}">
             <meta property="og:image" content="{{ get_seo_image($content) }}">
         @endpush
 
@@ -18,7 +18,7 @@
             <meta property="twitter:card" content="summary_large_image">
             <meta property="twitter:url" content="https://">
             <meta property="twitter:title" content="{{ $content->title }} | {{ get_website_setting('website.title') }}">
-            <meta property="twitter:description" content="{{ get_seo_description($content, get_theme_settings('seo.meta.description')) }}">
+            <meta property="twitter:description" content="{{ get_seo_description($content, get_theme_setting('seo.meta.description')) }}">
             <meta property="twitter:image" content="{{ get_seo_image($content) }}">
         @endpush
     @break

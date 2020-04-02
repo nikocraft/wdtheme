958894780d7d3950ebf326f60a5f17756cd2e189
nikocraft <niko@reimaginedworks.com>
2020-04-02 02:12:23 +0200

    work started on new theme look...

--- src/views/content/template/default/index.blade.php
+++ src/views/content/template/default/index.blade.php
@@ -1,52 +1,31 @@
 <div class="posts">
     @foreach ( $posts as $post )
         <div class="post">
-            <{{ get_theme_setting('content.general.postTitle.size') }} class="post-title"><a class="post-title-link" href="/{{ $post->type->slug }}/{{ $post->slug }}">{{ $post->title }}</a></{{ get_theme_setting('content.general.postTitle.size') }}>
+            <div class="post-content">
+                <{{ get_theme_setting('content.general.postTitle.size') }} class="post-title"><a class="post-title-link" href="/{{ $post->type->slug }}/{{ $post->slug }}">{{ $post->title }}</a></{{ get_theme_setting('content.general.postTitle.size') }}>
 
-            <div class="post-meta">
-                <div class="post-meta-detail">
-                    Posted on {{ $post->created_at->format('Y-m-d') }} by 
-                    @if(get_website_setting('website.members.userDisplayName') == 'fullname')
-                        {{ $post->author->firstname }} {{ $post->author->lastname }}
-                    @else
-                        {{ $post->author->username }}
-                    @endif
+                <div class="post-excerpt">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
                 </div>
             </div>
-            @if($post->featuredimage && !empty(get_theme_setting('content.general.featuredImage.indexPageHeight')))
-                <a class="post-image-link" href="/{{ $post->type->slug }}/{{ $post->slug }}">
-                    <div class="post-featured-image" style='background-image: url({{ $post->featuredimage->original }});'></div>
-                </a>
-                {{-- <img src="{{ $post->featuredimage->original }}" class="img-responsive" alt=""> --}}
-            @elseif($post->featuredimage && empty(get_theme_setting('content.general.featuredImage.indexPageHeight')))
-                <a class="post-image-link" href="/{{ $post->type->slug }}/{{ $post->slug }}">
-                    <img src="{{ $post->featuredimage->original }}" class="post-featured-image img-responsive">
-                </a>
-            @endif
 
-            @if(has_excerpt($post))
-                <div class="post-excerpt">
-                    {!! get_excerpt($post, get_theme_setting('content.general.excerptLength')) !!}
-                </div>
-            @elseif(has_text_block($post))
-                <div class="post-excerpt text-block">
-                    {!! get_text_block($post) !!}
-                </div>
-            @endif
-
-            <div class="post-footer">
-                <div class="post-meta-detail">
-                    <div class="post-taxonomy">
-                        @taxonomy([
-                            'taxonomy' => 'Tags',
-                            'post' => $post,
-                            'commaSeparate' => false
-                        ]) @endtaxonomy
+            <div class="post-meta">
+                <div class="post-author">
+                    <div class="post-author-image"><img style="width: 50px;" class="img-responsive" src="{{ get_gravatar($post->author->email) }}" /></div>
+                    <div class="post-author-detail">
+                        <div class="post-author-label">
+                            Author
+                        </div>
+                        <div class="post-author-name">{{ $post->author->firstname }} {{ $post->author->lastname }}</div>
                     </div>
                 </div>
-                @if(has_excerpt($post) || has_text_block($post))
-                    <a class="btn btn-primary" href="/{{ $post->type->slug }}/{{ $post->slug }}">Read More</a>
-                @endif
+                <div class="post-taxonomy">
+                    @taxonomy([
+                        'taxonomy' => 'Tags',
+                        'post' => $post,
+                        'commaSeparate' => false
+                    ]) @endtaxonomy
+                </div>
             </div>
         </div>
     @endforeach

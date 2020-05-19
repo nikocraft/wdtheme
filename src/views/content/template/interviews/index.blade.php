62149153f77b8853c45637bccf1cb280f27bd3e4
nikocraft <niko@reimaginedworks.com>
2020-05-19 18:11:59 +0200

    interview index and single pages visual improvements

--- src/views/content/template/interviews/index.blade.php
+++ src/views/content/template/interviews/index.blade.php
@@ -12,18 +12,6 @@
             <div class="post-content">
                 <h3 class="post-title"><a class="post-title-link" href="/{{ $post->type->slug }}/{{ $post->slug }}">{{ $post->title }}</a></h3>
             </div>
-
-            <div class="post-meta">
-                <div class="post-author">
-                    <div class="post-author-image"><img style="width: 50px;" class="img-responsive" src="{{ get_gravatar($post->author->email, 50, 'mp') }}" /></div>
-                    <div class="post-author-detail">
-                        <div class="post-author-label">
-                            Author
-                        </div>
-                        <div class="post-author-name">{{ $post->author->firstname }} {{ $post->author->lastname }}</div>
-                    </div>
-                </div>
-            </div>
         </div>
     @endforeach
 </div>
--- src/views/content/template/interviews/single.blade.php
+++ src/views/content/template/interviews/single.blade.php
@@ -18,12 +18,7 @@
             @if($showMetaData)
                 <div class="post-meta">
                     <div class="post-meta-detail">
-                        {{ $content->created_at->format('Y-m-d') }} by
-                        @if(get_website_setting('website.members.userDisplayName') == 'fullname')
-                            {{ $content->author->firstname }} {{ $content->author->lastname }}
-                        @else
-                            {{ $content->author->username }}
-                        @endif
+                        Posted on {{ $content->created_at->format('Y-m-d') }} by {{ $content->author->firstname }} {{ $content->author->lastname }}
                     </div>
                 </div>
             @endif
@@ -42,56 +37,15 @@
         @include('content/template/interviews/partials/content')
     </div>
 
-    {{-- @if($showAuthorBio)
-        <div class="post-author">
-            <div class="post-author-image"><img style="width: 120px;" class="img-responsive" src="{{ get_gravatar($content->author->email, 100, 'mp') }}" /></div>
-            <div class="post-author-details">
-                @if(get_website_setting('website.members.userDisplayName') == 'fullname')
-                    <div class="post-author-name">About {{ $content->author->firstname }} {{ $content->author->lastname }}</div>
-                @else
-                    <div class="post-author-name">About {{ $content->author->username }}</div>
-                @endif
-                <div class="post-author-bio">{{ $content->author->bio }}</div>
+    @if($showAuthorBio)
+        <a class="link-to-author" href="/user/nikocraft">
+            <div class="post-author">
+                <div class="post-author-image"><img class="img-responsive" src="{{ get_gravatar($content->author->email, 90, 'mp') }}" /></div>
+                <div class="post-author-detail">
+                    <div class="post-author-name">{{ $content->author->firstname }} {{ $content->author->lastname }}</div>
+                    <div class="post-author-bio">{{ $content->author->bio }}</div>
+                </div>
             </div>
-        </div>
-    @endif --}}
+        </a>
+    @endif
 </div>
-
-
-@if($showComments)
-    @switch(get_website_setting('website.comments.type'))
-        @case('native')
-            <comments
-                :allow-nested={{ json_encode(get_website_setting('website.comments.allowNested')) }}
-                :max-depth="{{ get_website_setting('website.comments.nestedDepth') }}"
-                order="{{ get_website_setting('website.comments.order') }}"
-                :content-id={{ $content->id }}
-                :content-author-id={{ $content->author->id }}>
-            </comments>
-            @break
-        @case('disqus')
-            @if(!empty(get_website_setting('website.comments.disqusChannel')))
-                <div id="disqus_thread"></div>
-                @push('scripts')
-                    <script id="disqus_comments">
-                        var disqus_config = function () {
-                            this.page.url = '{{ route("frontend.posts.show", $content->slug) }}';  // Replace PAGE_URL with your page's canonical URL variable
-                            this.page.identifier = '{{ $content->id }}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-                        };
-                        (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
-                            var d = document, s = d.createElement('script');
-                            
-                            s.src = 'https://{{ get_website_setting('website.comments.disqusChannel') }}.disqus.com/embed.js';  // IMPORTANT: Replace EXAMPLE with your forum shortname!
-                            
-                            s.setAttribute('data-timestamp', +new Date());
-                            (d.head || d.body).appendChild(s);
-                        })();
-                    </script>
-                    <script id="dsq-count-scr" src="//{{ get_website_setting('website.comments.disqusChannel') }}.disqus.com/count.js" async></script>
-                @endpush
-            @endif
-            @break
-        @default
-            {{-- defaults to no comments! --}}
-    @endswitch
-@endif
